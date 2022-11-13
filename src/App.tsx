8560d2a4710ca1f2beaa1b403e3e19da2ed009d6
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-13 15:19:36 -0300

    Adding a funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useContext, createContext } from "react";
 import "./Styles/App.scss";
 import { Button, ButtonGroup } from "react-bootstrap";
 import Fade from "react-bootstrap/Fade";
 export default function App() {
   const [number, setNumber] = useState<number>(0);
   const [open, setOpen] = useState<boolean>(false);
-  const [isEven, setIsEven] = useState<boolean>(false);
+  const [isEven, setIsEven] = useState<boolean>(true);
+  const checkNumber = createContext<number>(number);
+
+  function CheckIsEven(): any {
+    const number: number = useContext(checkNumber);
+    if (number % 2 === 0) {
+      return (
+        <>
+          <p>You won!</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p>You lose! Better luck next time.</p>
+        </>
+      );
+    }
+  }
+
+  function CheckIsOdd(): any {
+    const number: number = useContext(checkNumber);
+
+    if (number % 2 !== 0) {
+      return (
+        <>
+          <p>You won!</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p>You lose! Better luck next time.</p>
+        </>
+      );
+    }
+  }
+
   return (
     <>
-      <div className="">
-        <p className="h1">Guess if the number is Even or Odd</p>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <p className="h1 col-auto p-5 text-center">Is Even or Odd</p>
         <ButtonGroup
           onClick={() => {
             setOpen(!open);
@@ -19,26 +56,38 @@ export default function App() {
           }}
           aria-controls="testing"
           aria-expanded={open}
-          className=""
+          className="grid gap-4"
         >
-          <Button className="">Even</Button>
-          <Button onClick={() => setIsEven(!isEven)} className="">
-            Odd
+          <Button onClick={() => setIsEven(!false)} className="btn">
+            Even
           </Button>
+          <button
+            onClick={() => setIsEven(!true)}
+            className="btn border-solid tx-bg-orange-600"
+          >
+            Odd
+          </button>
         </ButtonGroup>
         <Fade in={open}>
-          <p className="">{number}</p>
+          <p className="h1 font-bold text-xl mb-2 col-auto p-5 text-center">
+            {number}
+          </p>
         </Fade>
         <Fade in={open}>
-          <Button
+          <button
             onClick={() => {
-              setIsEven(!isEven);
               setOpen(!open);
+              setIsEven(isEven == true);
             }}
             className=""
           >
             Reset
-          </Button>
+          </button>
+        </Fade>
+        <Fade in={open}>
+          <checkNumber.Provider value={number}>
+            {isEven === true ? <CheckIsEven /> : <CheckIsOdd />}
+          </checkNumber.Provider>
         </Fade>
       </div>
     </>
